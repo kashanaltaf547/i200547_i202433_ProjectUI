a88a7a9adb18c0a3a18fdbecfc232c2b825416db
KashanAltaf <kashanaltaf137@gmail.com>
2023-11-30 21:27:32 +0500

    Project UI

--- app/src/main/java/com/example/i200547_i202433_projectui/MainActivity2_SignIn.java
+++ app/src/main/java/com/example/i200547_i202433_projectui/MainActivity2_SignIn.java
@@ -1,14 +1,108 @@
 package com.example.i200547_i202433_projectui;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity2_SignIn extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
+public class MainActivity2_SignIn extends AppCompatActivity {
+    EditText username, pass;
+    Button b1, b2, b3;
+    FirebaseAuth mAuth;
+    ProgressBar progressBar;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity6_EditProfile.class);
+            startActivity(intent);
+            finish();
+        }
+    }
+
+    @SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_2_sign_in);
+        mAuth = FirebaseAuth.getInstance();
+
+        username = (EditText) findViewById(R.id.v8);
+        pass = (EditText) findViewById(R.id.v13);
+        b1 = (Button) findViewById(R.id.v14);
+        b2 = (Button) findViewById(R.id.v15);
+        b3 = (Button) findViewById(R.id.v21);
+        progressBar = (ProgressBar) findViewById(R.id.progress);
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Activity1();
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, password;
+                email = String.valueOf(username.getText());
+                password = String.valueOf(pass.getText());
+
+                if(TextUtils.isEmpty(email)){
+                    Toast.makeText(MainActivity2_SignIn.this, "Enter Name", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(TextUtils.isEmpty(password)){
+                    Toast.makeText(MainActivity2_SignIn.this, "Enter Email", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity2_SignIn.this, "Login Successful.",
+                                            Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(getApplicationContext(), MainActivity6_EditProfile.class);
+                                    startActivity(intent);
+                                    finish();
+                                } else {
+                                    // If sign in fails, display a message to the user.
+                                    Toast.makeText(MainActivity2_SignIn.this, "Login Failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
+
+    }
+
+
+
+    private void Activity1() {
+        Intent intent = new Intent(this, MainActivity3_ForgotPass.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/i200547_i202433_projectui/MainActivity3_ForgotPass.java
+++ app/src/main/java/com/example/i200547_i202433_projectui/MainActivity3_ForgotPass.java
@@ -1,14 +1,66 @@
 package com.example.i200547_i202433_projectui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ktx.Firebase;
+
 public class MainActivity3_ForgotPass extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    ImageButton i1;
+    EditText e1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_3_forgot_pass);
+
+        i1 = (ImageButton) findViewById(R.id.a15);
+        e1 = (EditText) findViewById(R.id.a9);
+
+        i1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String Email = e1.getText().toString().trim();
+                if(!TextUtils.isEmpty(Email)){
+
+                    i1.setVisibility(View.INVISIBLE);
+
+                    mAuth.sendPasswordResetEmail(Email).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(MainActivity3_ForgotPass.this, "Reset Password Link has been sent to the Email!", Toast.LENGTH_LONG).show();
+                                    Intent intent = new Intent(MainActivity3_ForgotPass.this, MainActivity2_SignIn.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(MainActivity3_ForgotPass.this, "Error in resetting password!", Toast.LENGTH_LONG).show();
+                                    i1.setVisibility(View.VISIBLE);
+                                }
+                            });
+                }
+                else{
+                    e1.setError("Please fill the email!");
+                }
+            }
+        });
+
     }
 }
